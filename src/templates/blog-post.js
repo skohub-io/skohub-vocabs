15d4c5f92e0217331177504d106c86ad8edd1c03
Daniel Doblado <danieldoblado@gmail.com>
2019-03-27 12:29:20 +0100

    Fix linter problems

--- src/templates/blog-post.js
+++ src/templates/blog-post.js
@@ -2,7 +2,7 @@ import React from "react"
 import Layout from "../components/layout"
 import { graphql } from "gatsby"
 
-export default ({ data }) => {
+const blogPost = ({ data }) => {
 
   const post = data.dataJson
 
@@ -30,3 +30,5 @@ export const query = graphql`
       }
   }
 }`
+
+export default blogPost
