5c11e2eeb1fab72ebf62a439c6162418d466107e
Daniel Doblado <danieldoblado@gmail.com>
2019-03-27 11:25:32 +0100

    Programmatically create new pages from json

--- gatsby-node.js
+++ gatsby-node.js
@@ -4,4 +4,44 @@
  * See: https://www.gatsbyjs.org/docs/node-apis/
  */
 
-// You can delete this file if you're not using it
+const path = require(`path`)
+const { createFilePath } = require(`gatsby-source-filesystem`)
+
+exports.onCreateNode = ({ node, getNode, actions }) => {
+  if (node.internal.type === 'DataJson') {
+    const { createNodeField } = actions
+    const slug = createFilePath({ node, getNode, basePath: `pages` })
+    createNodeField({
+      node,
+      name: `slug`,
+      value: slug
+    })
+   }
+}
+
+exports.createPages = ({ graphql, actions }) => {
+  const { createPage } = actions
+  return graphql(`
+    {
+      allDataJson {
+        edges {
+          node {
+          fields {
+            slug
+          }
+        }
+      }
+    }
+  }
+`).then(result => {
+  console.log(JSON.stringify(result, null, 4))
+  result.data.allDataJson.edges.forEach(({ node }) => {
+    createPage({
+      path: node.fields.slug,
+      component: path.resolve(`./src/templates/blog-post.js`),
+      context: {
+        slug: node.fields.slug,
+      },
+    })
+  })
+})}
---
+++ src/templates/blog-post.js
@@ -0,0 +1,32 @@
+import React from "react"
+import Layout from "../components/layout"
+import { graphql } from "gatsby"
+
+export default ({ data }) => {
+
+  const post = data.dataJson
+
+  return (
+    <Layout>
+     <pre style={{
+         whiteSpace: 'pre-wrap',
+         wordWrap: 'break-word'
+     }}>
+       {JSON.stringify(post, null, 2)}
+    </pre>
+    </Layout>
+  )
+}
+
+export const query = graphql`
+  query($slug: String!) {
+    dataJson(fields: { slug: { eq: $slug } }) {
+      _id
+      _context
+      _type
+      hasTopConcept {
+        _id
+        _type
+      }
+  }
+}`
