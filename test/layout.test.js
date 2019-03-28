8d5e10c28d911ac2d32e8ec316c90a7cecdbe19c
Daniel Doblado <danieldoblado@gmail.com>
2019-03-28 15:14:14 +0100

    Add test for layout

---
+++ test/layout.test.js
@@ -0,0 +1,49 @@
+/* global expect */
+/* global describe */
+/* global test */
+/* global beforeEach */
+
+import React from 'react'
+import Enzyme, { shallow } from 'enzyme'
+import Adapter from 'enzyme-adapter-react-16'
+import { StaticQuery } from 'gatsby';
+import Layout from '../src/components/layout'
+
+Enzyme.configure({ adapter: new Adapter() })
+
+beforeEach(() => {
+  StaticQuery.mockImplementationOnce(({ render }) =>
+    render({
+      site: {
+        siteMetadata: {
+          title: `Default Starter`,
+        },
+      },
+    })
+  )
+})
+
+const data = {
+  site: {
+    siteMetadata: {
+      title: "Gatsby Starter Blog",
+    },
+  },
+}
+
+describe('Layout', () => {
+  const wrapper = shallow(
+    <Layout data={data}>
+      <div>Test Layout</div>
+    </Layout>
+  )
+
+  test('Renders', () => {
+    const html = wrapper.html()
+    expect(wrapper.exists()).toBe(true)
+    expect(html.includes('header')).toBe(true)
+    expect(html.includes('Test Layout')).toBe(true)
+    expect(html.includes('footer')).toBe(true)
+  })
+
+})
