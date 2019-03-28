f0aa9b9b50bb6a6a63c48b5ec1e61934b1e0dff8
Daniel Doblado <danieldoblado@gmail.com>
2019-03-28 13:40:52 +0100

    Install jest packages and configure mocks

--- jest.config.js
+++ jest.config.js
@@ -1,7 +1,15 @@
 module.exports = {
-  setupFiles: ['<rootDir>/node_modules/regenerator-runtime/runtime'],
-  testPathIgnorePatterns: ['node_modules', '.cache'],
   transform: {
-    "^.+\\.jsx?$": "<rootDir>/jest-preprocess.js"
-  }
+    "^.+\\.jsx?$": `<rootDir>/jest-preprocess.js`,
+  },
+  moduleNameMapper: {
+    ".+\\.(css|styl|less|sass|scss)$": `identity-obj-proxy`,
+    ".+\\.(jpg|jpeg|png|gif|eot|otf|webp|svg|ttf|woff|woff2|mp4|webm|wav|mp3|m4a|aac|oga)$": `<rootDir>/__mocks__/file-mock.js`,
+  },
+  testPathIgnorePatterns: [`node_modules`, `.cache`],
+  transformIgnorePatterns: [`node_modules/(?!(gatsby)/)`],
+  globals: {
+    __PATH_PREFIX__: ``,
+  },
+  testURL: `http://localhost`,
 }
