ace586408c2755ee6f9c3a85495bfca891b0a467
Daniel Doblado <danieldoblado@gmail.com>
2019-05-27 15:20:20 +0200

    Set new folder for files

--- gatsby-config.js
+++ gatsby-config.js
@@ -6,7 +6,6 @@ module.exports = {
   },
   plugins: [
     `gatsby-plugin-emotion`,
-    `gatsby-plugin-react-helmet`,
     {
       resolve: `gatsby-source-filesystem`,
       options: {
@@ -17,23 +16,10 @@ module.exports = {
     `gatsby-transformer-sharp`,
     `gatsby-plugin-sharp`,
     {
-      resolve: `gatsby-plugin-manifest`,
-      options: {
-        name: `gatsby-starter-default`,
-        short_name: `starter`,
-        start_url: `/`,
-        background_color: `#663399`,
-        theme_color: `#663399`,
-        display: `minimal-ui`,
-        icon: `src/images/gatsby-icon.png`, // This path is relative to the root of the site.
-      },
-    },
-    `gatsby-transformer-json`,
-    {
       resolve: `gatsby-source-filesystem`,
       options: {
         name: 'data',
-        path: `./src/data/`,
+        path: `./src/data/ttl`,
       },
     },
     // this (optional) plugin enables Progressive Web App + Offline functionality
