c108f33131912ce0307d1a2c15cf0f8580a909b6
Daniel Doblado <danieldoblado@gmail.com>
2019-05-29 23:18:16 +0200

    GO back to test file

--- src/scripts/pullFiles.js
+++ src/scripts/pullFiles.js
@@ -3,7 +3,6 @@ const fs = require("fs")
 const { promisify } = require('util')
 
 const writeFile = promisify(fs.writeFile)
-console.log("ENVIROMENT", process.env && process.env.INCOMING_HOOK_BODY)
 
 const repository = process.env.INCOMING_HOOK_BODY ? JSON.parse(decodeURI(process.env.INCOMING_HOOK_BODY)).repository.full_name : "dobladov/testTTTL"
 
@@ -25,7 +24,7 @@ const getFile = async (file) => {
   try {
     const response = await fetch(file.url)
     const data = await response.text()
-    await writeFile(`/tmp/ttl/${file.name}`, data)
+    await writeFile(`test/data/${file.name}`, data)
     console.log("Created file:", file.name)
   } catch (error) {
     console.log(error)
