90c2193cfad0916cea87e51a74646af2d17a5ce4
Daniel Doblado <danieldoblado@gmail.com>
2019-05-29 17:10:59 +0200

    Add script to fetch files from remote repository and update build script

---
+++ src/scripts/pullFiles.js
@@ -0,0 +1,36 @@
+const fetch = require("node-fetch")
+const fs = require("fs")
+const { promisify } = require('util')
+
+const writeFile = promisify(fs.writeFile)
+const repository = process.env.INCOMING_HOOK_BODY.repository.full_name
+
+console.log(process.env)
+
+const pullFiles = async () => {
+  const response = await fetch(`https://api.github.com/repositorys/${repository}/contents/data`)
+  const json = await response.json()
+
+  const files = json.map(file => {
+    return {url: file.download_url, name: file.name}
+  })
+
+  for (const file of files) {
+    getFile(file)
+  }
+}
+
+const getFile = async (file) => {
+  try {
+    const response = await fetch(file.url)
+    const data = await response.text()
+    await writeFile(`test/data/${file.name}`, data)
+    console.log("Created file:", file.name)
+  } catch (error) {
+    console.log(error)
+  }
+}
+
+if (repository) {
+  pullFiles()
+}
