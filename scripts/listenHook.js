bcad395b22fd86e53bfcd0886c3f9801dfb77d4a
Daniel Doblado <danieldoblado@gmail.com>
2019-07-24 17:32:38 +0200

    Add logic for webhooks

--- scripts/listenHook.js
+++ scripts/listenHook.js
@@ -3,55 +3,107 @@ const Koa = require('koa')
 const Router = require('koa-router')
 const bodyParser = require('koa-bodyparser')
 const fetch = require("node-fetch")
-const { promisify } = require('util')
-
+const crypto = require('crypto')
+const uuidv4 = require('uuid/v4')
+const fs = require('fs-extra')
 const exec = require('child_process').exec
-const writeFile = promisify(require("fs").writeFile)
+require('dotenv').config()
+require('colors')
 
-const port = 3000
+const { PORT, SECRET } = process.env
 const app = new Koa()
 const router = new Router()
 
-const getFile = async (file) => {
+const webhooks = []
+let processingWebhooks = false
+
+const getFile = async (file, repository) => {
   try {
     const response = await fetch(file.url)
     const data = await response.text()
-    await writeFile(`test/data/${file.name}`, data)
-    console.info("Created file:", file.name)
+    const path = `data/${repository}/`
+    await fs.outputFile(`${path}${file.name}`, data)
+    console.info("Created file:".green, file.name)
   } catch (error) {
     console.error(error)
   }
 }
 
-router.post('/build', async (ctx, next) => {
-  await next()
-  const body = ctx.request.body
+const isSecured = (signature, payload) => {
+  const hmac = crypto.createHmac('sha1', SECRET)
+  const digest = 'sha1=' + hmac.update(JSON.stringify(payload)).digest('hex')
+  console.log(signature, digest)
+  return signature === digest
+}
+
+router.post('/build', async (ctx) => {
+  const { body } = ctx.request
+  const signature = ctx.request.headers['x-hub-signature']
   const repository = (body && body.repository && body.repository.full_name ) || null
-  if (repository) {
-    const response = await fetch(`https://api.github.com/repos/${repository}/contents/data`)
-    const json = await response.json()
 
-    const files = json.map(file => {
-      return {url: file.download_url, name: file.name}
-    })
+  // Check if the given signature is valid
+  if (isSecured(signature, body)) {
+    if (repository) {
 
-    for (const file of files) {
-      await getFile(file)
+      webhooks.push({
+        id: uuidv4(),
+        signature,
+        body,
+        repository,
+        date: new Date().toISOString()
+      })
+
+      ctx.body = 'Files Created'
+    } else {
+      ctx.status = 400
+      ctx.message = 'Error creating the files'
     }
-    ctx.body = 'Files Created'
-
-    const build = exec('npm run build')
-    build.stdout.on('data', (data) => console.log('gatsbyLog: ' + data.toString()))
-    build.stderr.on('data', (data) => console.log('gatsbyError: ' + data.toString()))
-    build.on('exit', (code) => console.info("Build Finish"))
   } else {
-    ctx.status = 400
-    ctx.message = 'Error creating the files'
+    console.warn("The signature does not match")
   }
 })
+
+const processWebhook = async (webhook) => {
+  const response = await fetch(`https://api.github.com/repos/${webhook.repository}/contents/data`)
+  const json = await response.json()
+
+  const files = json.map(file => {
+    return {
+      url: file.download_url, name: file.name
+    }
+  })
+
+  for (const file of files) {
+    await getFile(file, webhook.repository)
+  }
+}
+
+const processWebhooks = async () => {
+  if (processingWebhooks === false) {
+    if (webhooks.length > 0) {
+      processingWebhooks = true
+      console.log(`Processing`.green)
+      const webhook = webhooks.shift()
+      await processWebhook(webhook)
+
+      const build = exec('npm run build')
+      build.stdout.on('data', (data) => console.log('gatsbyLog: ' + data.toString()))
+      build.stderr.on('data', (data) => console.log('gatsbyError: ' + data.toString()))
+      build.on('exit', (code) => {
+        console.info("Build Finish".yellow)
+        processingWebhooks = false
+      })
+    }
+  }
+}
 
 app
   .use(bodyParser())
   .use(router.routes())
   .use(router.allowedMethods())
-  .listen(port, () => console.info(`⚡ Listening on localhost:${port}`))
+  .listen(PORT, () => console.info(`⚡ Listening on localhost:${PORT}`))
+
+// Loop to processing requests
+setInterval(() => {
+  processWebhooks()
+}, 1)
