287a99245dc6f5d7a454bd8f2d6f6dd37721068b
Felix Ostrowski <felix.ostrowski@googlemail.com>
2019-07-25 12:40:16 +0200

    Support namespaced source directories

--- gatsby-config.js
+++ gatsby-config.js
@@ -19,7 +19,7 @@ module.exports = {
       resolve: `gatsby-source-filesystem`,
       options: {
         name: 'data',
-        path: `${__dirname}/data`,
+        path: `${__dirname}/data/${process.env.GITHUB_REPOSITORY || ''}`,
       },
     },
     // this (optional) plugin enables Progressive Web App + Offline functionality
