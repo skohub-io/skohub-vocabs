1afaa8ff34467d41e6c01636ac21e7417f2fb0ba
Felix Ostrowski <felix.ostrowski@googlemail.com>
2019-08-08 16:28:11 +0200

    Ensure filename, support https URLs

--- src/common.js
+++ src/common.js
@@ -1,4 +1,9 @@
 module.exports.t = localized => localized
   && (Object.entries(localized).filter(([, value]) => !!value).shift() || []).pop()
   || ''
-module.exports.getPath = (url, extension) => url.replace("http:/", "").replace("#", "") + '.' + extension
+
+module.exports.getPath = (url, extension) => {
+  let path = url.replace(/^https?:\//, "").replace(/#$/, "")
+  path.endsWith('/') && (path += 'index')
+  return `${path}.${extension}`
+}
