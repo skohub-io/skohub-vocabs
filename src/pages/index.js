e04043ae1311ae813d58120c8d272c3cd9d58f94
Daniel Doblado <danieldoblado@gmail.com>
2019-09-05 14:29:36 +0200

    Return id if title is not present

--- src/pages/index.js
+++ src/pages/index.js
@@ -8,15 +8,18 @@ import SEO from "../components/seo"
 const IndexPage = ({ data }) => {
 
   const { edges } = data.allConceptScheme
-  console.log(edges)
+  const conceptSchemes = edges.map(node => node.node)
 
   return (
   <Layout>
     <SEO title="Home" keywords={[`gatsby`, `application`, `react`]} />
     <ul>
-      {edges.map(node => (
-        <li key={node.node.id}>
-          <Link to={(process.env.BASEURL || '') + getPath(node.node.id, 'html')}>{t(node.node.title)}</Link>
+      {conceptSchemes.map(conceptScheme => (
+        <li key={conceptScheme.id}>
+          <Link
+            to={(process.env.BASEURL || '') + getPath(conceptScheme.id, 'html')}>{conceptScheme.title
+              ? t(conceptScheme.title) : conceptScheme.id}
+          </Link>
         </li>
       ))}
     </ul>
@@ -32,6 +35,7 @@ export const query = graphql`
           title {
             de
             en_us
+            en
           }
         }
       }
