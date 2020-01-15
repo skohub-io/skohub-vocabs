a1230059a0242e984a94194bb20e5bc71b733a9a
Felix Ostrowski <felix.ostrowski@googlemail.com>
2020-01-15 17:29:34 +0100

    Add link directives
    Fixes deprecation warnings

--- src/types.js
+++ src/types.js
@@ -3,7 +3,7 @@ module.exports = languages => `
     type: String,
     title: LanguageMap,
     description: LanguageMap,
-    hasTopConcept: [Concept]
+    hasTopConcept: [Concept] @link(from: "hasTopConcept___NODE")
   }
 
   type Concept implements Node {
@@ -13,12 +13,12 @@ module.exports = languages => `
     scopeNote: LanguageMap,
     note: LanguageMap,
     notation: [String],
-    topConceptOf: ConceptScheme,
-    narrower: [Concept],
-    narrowerTransitive: [Concept],
-    broader: Concept,
-    broaderTransitive: [Concept],
-    inScheme: ConceptScheme!,
+    topConceptOf: ConceptScheme @link(from: "topConceptOf___NODE"),
+    narrower: [Concept] @link(from: "narrower___NODE"),
+    narrowerTransitive: [Concept] @link(from: "narrowerTransitive___NODE"),
+    broader: Concept @link(from: "broader___NODE"),
+    broaderTransitive: [Concept] @link(from: "broaderTransitive___NODE"),
+    inScheme: ConceptScheme! @link(from: "inScheme___NODE"),
     hub: String,
     inbox: String
   }
