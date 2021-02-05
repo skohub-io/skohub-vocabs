5db928ba5b4752e45f9a8b58f83a0209fc164067
@s.roertgen <sroertgen@gmail.com>
2021-02-05 13:03:00 +0100

    add baseurl to JSON Link component

--- src/components/JsonLink.js
+++ src/components/JsonLink.js
@@ -2,7 +2,7 @@ import React from 'react'
 import jsonpng from '../images/jsonld.png'
 
 const JsonLink = ({to}) => (
-  <a className="json-png" href={to}>
+  <a className="json-png" href={(process.env.BASEURL || "") + to}>
     <img src={jsonpng} alt="JSON" />
   </a>
 )
