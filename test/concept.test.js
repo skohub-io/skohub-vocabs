c75311b0c75a556e31316d85b9d9a51a535938f5
@s.roertgen <sroertgen@gmail.com>
2022-03-08 17:40:11 +0100

    update test config
    see here for details: https://www.gatsbyjs.com/docs/how-to/testing/unit-testing/#2-creating-a-configuration-file-for-jest

--- test/concept.test.js
+++ test/concept.test.js
@@ -5,7 +5,7 @@
 
 import React from 'react'
 import Enzyme, { shallow } from 'enzyme'
-import Adapter from 'enzyme-adapter-react-16'
+import Adapter from '@wojtekmaj/enzyme-adapter-react-17'
 import pageContext from '../test/data/pageContext'
 import Concept from '../src/components/Concept'
 import { StaticQuery } from 'gatsby';
