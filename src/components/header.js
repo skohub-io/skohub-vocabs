6f891a9bb2367601d854c6614f1d6e48e88700fb
@s.roertgen <sroertgen@gmail.com>
2022-12-13 14:41:18 +0100

    don't show language tags in vocabs that do not have that language

--- src/components/header.js
+++ src/components/header.js
@@ -1,8 +1,9 @@
 import { Link } from "gatsby"
 import { css } from '@emotion/react'
 import PropTypes from "prop-types"
-import React from "react"
+import React, { useEffect, useState } from "react"
 import { useLocation } from "@gatsbyjs/reach-router"
+import { getFilePath } from '../common'
 
 import { colors as c } from '../styles/variables'
 import skohubsvg from '../images/skohub-signet-color.svg'
@@ -82,20 +83,72 @@ const style = css`
     }
   }
 `
-const Header = ({ siteTitle, languages, language, pathName = useLocation().pathname.slice(0, -8) }) => (
-  <header
-    css={style}
-  >
+
+
+const Header = ({ siteTitle, languages, language }) => {
+  const [conceptScheme, setConceptScheme] = useState({})
+  const [langs, setLangs] = useState(new Set())
+  const pathName = useLocation().pathname.slice(0, -8)
+
+  // to display the concept scheme title in the header
+  // we have to retrieve concept scheme info in this component
+  useEffect(() => {
+    function parseLanguages(arrayOfObj) {
+      for (let obj of arrayOfObj) {
+        for (let k of Object.keys(obj)) {
+          if (k === "hasTopConcept" || k === "narrower") {
+            // Concept Schemes
+            obj?.title && Object.keys(obj.title).forEach(t => setLangs(prev => new Set(prev.add(t))))
+            // Concepts
+            obj?.prefLabel && Object.keys(obj.prefLabel).forEach(t => setLangs(prev => new Set(prev.add(t))))
+            obj?.altLabel && Object.keys(obj.altLabel).forEach(t => setLangs(prev => new Set(prev.add(t))))
+            obj?.hiddenLabel && Object.keys(obj.hiddenLabel).forEach(t => setLangs(prev => new Set(prev.add(t))))
+
+            obj?.hasTopConcept && parseLanguages(obj?.hasTopConcept)
+            obj?.narrower && parseLanguages(obj?.narrower)
+          }
+        }
+      }
+    }
+    fetch(pathName + ".json")
+      .then((response) => response.json())
+      .then((r) => {
+        if (r.type === "ConceptScheme") {
+          setConceptScheme((prev) => ({ ...prev, ...r }))
+          parseLanguages([r])
+        } else if (r.type === "Concept") {
+          const cs = r.topConceptOf ? r.topConceptOf : r.inScheme
+          setConceptScheme((prev) => ({ ...prev, ...cs }))
+          fetch(getFilePath(cs.id, "json"))
+            .then((response) => response.json())
+            .then((r) => {
+              parseLanguages([r])
+            })
+        } else {
+          languages.forEach(l => setLangs(prev => new Set(prev.add(l))))
+        }
+      })
+  }, [pathName, languages])
+
+  return (
+    <header css={style}>
     <div className="headerContent">
       <div className="skohubLogo">
-          <Link to={`/index.${language}.html`} >
+          <Link to={`/index.${language}.html`}>
           <img className="skohubImg" src={skohubsvg} alt="SkoHub" />
           <span className="skohubTitle">{siteTitle}</span>
         </Link>
+        {conceptScheme && conceptScheme.id && (
+          <div className="conceptScheme">
+          <Link to={getFilePath(conceptScheme.id, `${language}.html`)}>
+            {conceptScheme?.title?.language || conceptScheme.id}
+          </Link>
+          </div>
+        )}
       </div>
-      {languages && languages.length > 1 && (
+      {langs && Array.from(langs).length > 1 && (
         <ul className="language-menu">
-          {languages.map(l => (
+            {Array.from(langs).map((l) => (
             <li key={l}>
               {l === language ? (
                 <span className="currentLanguage">{l}</span>
@@ -107,9 +160,9 @@ const Header = ({ siteTitle, languages, language, pathName = useLocation().pathn
         </ul>
       )}
     </div>
-
   </header>
 )
+}
 
 Header.propTypes = {
   siteTitle: PropTypes.string,
