d320d8958f1b3651f19f495f8736e958d971860c
@s.roertgen <sroertgen@gmail.com>
2022-12-20 17:06:06 +0100

    remove unnecessary imports

--- test/header.test.js
+++ test/header.test.js
@@ -1,16 +1,12 @@
 import React from "react"
 import { render, screen, act } from "@testing-library/react"
-import pageContext from "../test/data/pageContext"
 import Header from "../src/components/header"
 import mockFetch from "./mocks/mockFetch"
 import {
-  Router,
-  Link,
   createHistory,
   createMemorySource,
   LocationProvider,
 } from "@gatsbyjs/reach-router"
-// import { act } from "react-dom/test-utils"
 
 beforeEach(() => {
   jest.spyOn(window, "fetch").mockImplementation(mockFetch)
