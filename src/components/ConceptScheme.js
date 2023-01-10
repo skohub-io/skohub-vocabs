8b5f60f81c049d8e81c04d759fe54bba28e09fad
@s.roertgen <sroertgen@gmail.com>
2023-01-10 14:04:19 +0100

    show prefLabel of hashURI concepts
    `language` parameter was missing, so I guess this was not working for a long time.
    will add a test for this as well

--- src/components/ConceptScheme.js
+++ src/components/ConceptScheme.js
@@ -8,26 +8,31 @@ import JsonLink from "./JsonLink"
 
 const ConceptScheme = ({
   pageContext: { node: conceptScheme, embed, language },
-}) => (
-  <div
-    className="content concept block main-block"
-    id={getDomId(conceptScheme.id)}
-  >
-    {embed &&
-      embed.map((concept) => (
-        <Concept key={concept.json.id} pageContext={{ node: concept.json }} />
-      ))}
-    <div>
-      <h1>{i18n(language)(conceptScheme.title)}</h1>
-      <h2>{conceptScheme.id}</h2>
-      <JsonLink to={getFilePath(conceptScheme.id, "json")} />
-      {conceptScheme.description && (
-        <div className="markdown">
-          <Markdown>{i18n(language)(conceptScheme.description)}</Markdown>
-        </div>
-      )}
+}) => {
+  return (
+    <div
+      className="content concept block main-block"
+      id={getDomId(conceptScheme.id)}
+    >
+      {embed &&
+        embed.map((concept) => (
+          <Concept
+            key={concept.json.id}
+            pageContext={{ node: concept.json, language }}
+          />
+        ))}
+      <div>
+        <h1>{i18n(language)(conceptScheme.title)}</h1>
+        <h2>{conceptScheme.id}</h2>
+        <JsonLink to={getFilePath(conceptScheme.id, "json")} />
+        {conceptScheme.description && (
+          <div className="markdown">
+            <Markdown>{i18n(language)(conceptScheme.description)}</Markdown>
+          </div>
+        )}
+      </div>
     </div>
-  </div>
-)
+  )
+}
 
 export default ConceptScheme
