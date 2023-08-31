a1c543c0503a02fc8e9e60cd920ff0b543d156f2
@s.roertgen <sroertgen@gmail.com>
2023-08-31 14:45:14 +0200

    removing interceptors, since this did not work as well
    adding a wait after the click like recommended here https://stackoverflow.com/a/76435762

--- cypress/e2e/searchAndFilter.cy.js
+++ cypress/e2e/searchAndFilter.cy.js
@@ -20,18 +20,10 @@ describe("search and filter", () => {
   })
 
   it("search works after switching language", () => {
-    cy.intercept("/w3id.org/search/en/prefLabel.cfg.json").as("cfg")
-    cy.intercept("/w3id.org/search/en/prefLabel.ctx.json").as("ctx")
-    cy.intercept("/w3id.org/search/en/prefLabel.map.json").as("map")
-    cy.intercept("/w3id.org/search/en/reg.json").as("reg")
-    // cy.intercept("/w3id.org/search/en/prefLabel.tag.json").as("tag")
-    cy.intercept("/w3id.org/search/en/prefLabel.store.json").as("store")
-    // cy.intercept("/w3id.org/index.en.html/page-data.json").as("pageData")
     cy.visit("/w3id.org/index.de.html")
 
-    cy.contains("en").click()
+    cy.contains("en").click().wait(0) // eslint-disable-line
 
-    cy.wait(["@ctx", "@cfg", "@map", "@reg", "@store"])
     cy.get(".currentLanguage").contains("en").should("exist")
     // cy.get("span").contains("Konzept 1").should("not.exist")
     cy.get("span", { timeout: 10000 }).contains("Concept 1").should("exist")
