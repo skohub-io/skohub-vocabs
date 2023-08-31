76756d9043b5903046e5800e4ac2fb9f5e6b0d53
@s.roertgen <sroertgen@gmail.com>
2023-08-31 14:40:27 +0200

    debugging github actions is annoying

--- cypress/e2e/searchAndFilter.cy.js
+++ cypress/e2e/searchAndFilter.cy.js
@@ -24,14 +24,14 @@ describe("search and filter", () => {
     cy.intercept("/w3id.org/search/en/prefLabel.ctx.json").as("ctx")
     cy.intercept("/w3id.org/search/en/prefLabel.map.json").as("map")
     cy.intercept("/w3id.org/search/en/reg.json").as("reg")
-    cy.intercept("/w3id.org/search/en/prefLabel.tag.json").as("tag")
+    // cy.intercept("/w3id.org/search/en/prefLabel.tag.json").as("tag")
     cy.intercept("/w3id.org/search/en/prefLabel.store.json").as("store")
     // cy.intercept("/w3id.org/index.en.html/page-data.json").as("pageData")
     cy.visit("/w3id.org/index.de.html")
 
     cy.contains("en").click()
 
-    cy.wait(["@ctx", "@cfg", "@map", "@reg", "@tag", "@store"])
+    cy.wait(["@ctx", "@cfg", "@map", "@reg", "@store"])
     cy.get(".currentLanguage").contains("en").should("exist")
     // cy.get("span").contains("Konzept 1").should("not.exist")
     cy.get("span", { timeout: 10000 }).contains("Concept 1").should("exist")
