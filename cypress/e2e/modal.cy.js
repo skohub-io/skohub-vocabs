91ea6fda85b11a60ed12ca9c439a57a07726a100
@s.roertgen <sroertgen@gmail.com>
2023-09-07 08:23:20 +0200

    add tests for closing modal

---
+++ cypress/e2e/modal.cy.js
@@ -0,0 +1,20 @@
+describe("modal", () => {
+  // closing modal works
+  it("click on close closes the modal", () => {
+    cy.visit("/w3id.org/index.de.html")
+    cy.get("#settingsModal").should("not.be.visible")
+    cy.get("#settings").click()
+    cy.get("#settingsModal").should("be.visible")
+    cy.get("#closeModal").click()
+    cy.get("#settingsModal").should("not.be.visible")
+  })
+
+  it("click outside closes the modal", () => {
+    cy.visit("/w3id.org/index.de.html")
+    cy.get("#settingsModal").should("not.be.visible")
+    cy.get("#settings").click()
+    cy.get("#settingsModal").should("be.visible")
+    cy.get("#settingsModal").click(-5, -5)
+    cy.get("#settingsModal").should("not.be.visible")
+  })
+})
