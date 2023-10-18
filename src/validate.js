6f92217ae1da7a833cf9044f917d589b319c0ddd
@s.roertgen <sroertgen@gmail.com>
2023-10-18 14:13:19 +0200

    Add boolean return statements

--- src/validate.js
+++ src/validate.js
@@ -1,43 +1,59 @@
-const fs = require('fs')
-const f = import('rdf-ext')
-const PN3 = import('@rdfjs/parser-n3')
-const shacl = import('rdf-validate-shacl')
+const fs = require("fs")
+const f = import("rdf-ext")
+const PN3 = import("@rdfjs/parser-n3")
+const shacl = import("rdf-validate-shacl")
 
-async function loadDataset (filePath) {
-  const ParserN3 = await PN3;
-  const factory = await f;
+async function loadDataset(filePath) {
+  const ParserN3 = await PN3
+  const factory = await f
   const stream = fs.createReadStream(filePath)
   const parser = new ParserN3.default({ factory: factory.default })
   return factory.default.dataset().import(parser.import(stream))
 }
 
+/**
+ * Validates a file against a given shape.
+ * @param {string} shapePath - Path to shape file
+ * @param {string} filePath - Path to file to validate
+ * @returns {boolean} Validation result
+ */
 async function validate(shapePath, filePath) {
-  const factory = await f;
+  const factory = await f
   const SHACLValidator = await shacl
 
   const shapes = await loadDataset(shapePath)
   const data = await loadDataset(filePath)
-  const validator = new SHACLValidator.default(shapes, { factory: factory.default })
-  const report = await validator.validate(data)
+  const validator = new SHACLValidator.default(shapes, {
+    factory: factory.default,
+  })
+  const report = validator.validate(data)
 
   // Check conformance: `true` or `false`
-  console.info("Validation result: ", report.conforms)
+  console.info("Validation result: ", report.conforms) // eslint-disable-line no-console
 
-  for (const result of report.results) {
-    // See https://www.w3.org/TR/shacl/#results-validation-result for details
-    // about each property
-    console.log(result.message)
-    console.log(result.path)
-    console.log(result.focusNode)
-    console.log(result.severity)
-    // console.log(result.sourceConstraintComponent)
-    // console.log(result.sourceShape)
+  if (report.conforms) {
+    return true
+  } else {
+    for (const result of report.results) {
+      // See https://www.w3.org/TR/shacl/#results-validation-result for details
+      // about each property
+      /* eslint-disable no-console */
+      console.log(result.message)
+      console.log(result.path)
+      console.log(result.focusNode)
+      console.log(result.severity)
+      // console.log(result.sourceConstraintComponent)
+      // console.log(result.sourceShape)
+      /* eslint-enable no-console */
+    }
+
+    // Validation report as RDF dataset
+    // console.log(report.dataset)
+
+    return false
   }
-
-  // Validation report as RDF dataset
-  // console.log(report.dataset)
 }
 
 module.exports = {
-  validate
+  validate,
 }
