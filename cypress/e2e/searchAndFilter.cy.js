d6fef4ef0f546dfb25351cd39fb4ef38023a8485
@s.roertgen <sroertgen@gmail.com>
2023-11-07 12:17:21 +0100

    Lets try an interceptor

--- cypress/e2e/searchAndFilter.cy.js
+++ cypress/e2e/searchAndFilter.cy.js
@@ -28,6 +28,7 @@ describe("search and filter", () => {
   })
 
   it("search works after switching language", () => {
+    cy.intercept("GET", "/w3id.org-cs/search/en/*").as("prefLabel")
     cy.visit("/w3id.org/index.html", {
       onBeforeLoad(win) {
         Object.defineProperty(win.navigator, "language", { value: "de-DE" })
@@ -35,6 +36,7 @@ describe("search and filter", () => {
     })
 
     cy.contains("en").click()
+    cy.wait("@prefLabel")
 
     // cy.get(".currentLanguage").contains("en").should("exist")
     cy.get("span").contains("Konzept 1").should("not.exist")
