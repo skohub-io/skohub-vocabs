70ae53c291e54fffbf076e6b39cfa071577df4e2
@s.roertgen <sroertgen@gmail.com>
2023-11-21 10:54:44 +0100

    Add e2e tests for hash URIs

--- cypress/e2e/conceptSchemeAndConcept.cy.js
+++ cypress/e2e/conceptSchemeAndConcept.cy.js
@@ -32,4 +32,45 @@ describe("Concept Scheme and Concept", () => {
       .then((clip) => clip.readText())
       .should("equal", "https://w3id.org/kim/hochschulfaechersystematik/n1")
   })
+
+  it("Visting a slash URI Concept Scheme works", () => {
+    cy.visit("/w3id.org/index.html", {
+      onBeforeLoad(win) {
+        Object.defineProperty(win.navigator, "language", { value: "de-DE" })
+      },
+    })
+    cy.get(".conceptScheme > a").should("have.text", "Test Vokabular")
+    cy.get("h1").should("have.text", "Test Vokabular")
+    cy.get(".markdown > span").should("have.text", "Test Beschreibung")
+  })
+
+  it("Visting a slash URI Concept works", () => {
+    cy.visit("/w3id.org/c1.html", {
+      onBeforeLoad(win) {
+        Object.defineProperty(win.navigator, "language", { value: "de-DE" })
+      },
+    })
+    cy.get(".conceptScheme > a").should("have.text", "Test Vokabular")
+    cy.get("h1").should("include.text", "Konzept 1")
+  })
+
+  it("Visting a hash URI Concept Scheme works", () => {
+    cy.visit("/example.org/hashURIConceptScheme.html", {
+      onBeforeLoad(win) {
+        Object.defineProperty(win.navigator, "language", { value: "de-DE" })
+      },
+    })
+    cy.get(".conceptScheme > a").should("have.text", "Hash URI Konzept Schema")
+    cy.get("h1").should("have.text", "Hash URI Konzept Schema")
+  })
+
+  it("Visting a hash URI Concept works", () => {
+    cy.visit("/example.org/hashURIConceptScheme.html#concept1", {
+      onBeforeLoad(win) {
+        Object.defineProperty(win.navigator, "language", { value: "de-DE" })
+      },
+    })
+    cy.get(".conceptScheme > a").should("have.text", "Hash URI Konzept Schema")
+    cy.get("h1").should("have.text", "Konzept 1")
+  })
 })
